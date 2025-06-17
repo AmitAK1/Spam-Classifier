3e75a7dbebf6b8e4b2b337f7b1faa045de31564f
AmitAK1 <amit.ak0599@gmail.com>
2025-06-17 16:00:17 +0530

    Update app.py
    F

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from nltk.stem import PorterStemmer
 ps = PorterStemmer()
 
 
-tfidf = joblib.load("vectorizer.pkl")
-model = joblib.load("model.pkl")
+tfidf = joblib.load("vectorizerF.pkl")
+model = joblib.load("modelF.pkl")
 
 st.title('Spam Classifier')
 input_msg=st.text_area("Enter the message to classify")
